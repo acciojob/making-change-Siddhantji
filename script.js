0dac55ecd8d04b71689d6ed177e69fb73a74b9ce
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-03-29 19:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let qua = 0;
+	let dime = 0;
+	let nickel = 0;
+	let penny =0;
+	qua = Math.floor(c/25);
+	c%=25;
+	dime = Math.floor(c/10);
+	c%=10;
+	nickel= Math.floor(c/5);
+	c%=5;
+	penny=c;
+	return{q: qua, d: dime,n: nickel,p:penny};
+	
 };
 
 // Do not the change the code below
